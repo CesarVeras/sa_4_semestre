110f6bcce7c1ecf9b6e5164e0cf77588024528ae
Eduardo Cesar Veras dos Santos <eduardocvsantos@gmail.com>
2018-11-19 21:14:05 -0200

    Inicio do desenvolvimento dos scritps de salas para o mapa do jogo e preparação da nova cena para futuras mudanças

---
+++ Assets/Scripts/MapManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapManager : MonoBehaviour
+{
+
+    public GameObject[] rooms;
+    public Queue<GameObject> rooms_queue = new Queue<GameObject>();
+    public GameObject currentRoom;
+    public int roomCounter;
+
+    // Use this for initialization
+    void Start()
+    {
+        foreach (GameObject r in rooms)
+        {
+            rooms_queue.Enqueue(r);
+            print(r.name);
+        }
+        print(GameObject.Find("Room 1").transform.Find("Wall D").name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void NextRoom()
+    {
+        if (rooms_queue.Count < 0)
+        {
+            return;
+        }
+        roomCounter++;
+        currentRoom = rooms_queue.Dequeue();
+    }
+
+    void MoveToCurrentRoom()
+    {
+        Camera.main.transform.position = currentRoom.transform.position;
+    }
+}
